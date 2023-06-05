7aee5fadb32cadd296a4218f8bafd1bd6a1eeb62
taylor xie <yyviper@vip.qq.com>
2023-06-05 17:18:43 +0800

    1.add

--- go.mod
+++ go.mod
@@ -3,5 +3,6 @@ module github.com/smallThank/gotest
 go 1.20
 
 retract v0.1.3
+retract v0.1.4
 //retract v0.1.1
 //retract v0.1.0
--- src/hello.go
+++ src/hello.go
@@ -2,6 +2,6 @@ package src
 
 import "fmt"
 
-func SayHi(content string) {
-	fmt.Println(content+",很高兴认识你", "haha, 智障吗你")
+func SayHi(content string, age uint8) {
+	fmt.Println(content+",很高兴认识你", "haha, 智障吗你, i am ", age, "year")
 }
